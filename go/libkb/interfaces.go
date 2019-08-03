0c7224253ca6299575bd9874cb397ec3e3bfb257
Michał Zochniak <michal@zapu.net>
2019-08-03 20:57:53 +0200

    wip - test passing

--- go/libkb/interfaces.go
+++ go/libkb/interfaces.go
@@ -1094,5 +1094,5 @@ type StandaloneChatConnector interface {
 type SyncedContactListProvider interface {
 	SaveProcessedContacts(MetaContext, []keybase1.ProcessedContact) error
 	RetrieveContacts(MetaContext) ([]keybase1.ProcessedContact, error)
-	ClearPhoneNumber(MetaContext, keybase1.PhoneNumber)
+	UnresolveContactsWithComponent(MetaContext, *keybase1.PhoneNumber, *keybase1.EmailAddress)
 }
